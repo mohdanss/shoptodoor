5bebcd8c597f843127508bbc9c6ebe9d51064399
Awais Bharwana <awaiswaheed134@gmail.com>
2022-12-22 15:48:28 +0500

    loginSignUpBackend

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.ATLAS_URI;
+
+exports.connect = () => {
+  // Connecting to the database
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
